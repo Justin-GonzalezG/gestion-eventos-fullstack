9613fc71239df0c558cf6d185d0d4931e283d547
Ben. <benjaminsalazar2111@gmail.com>
2026-04-14 17:54:10 -0400

    Delete microservices/src directory

--- microservices/src/main/java/com/eventos/cl/ms_usuarios/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventos.cl.ms_usuarios.repository;
-
-import com.eventos.cl.ms_usuarios.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
