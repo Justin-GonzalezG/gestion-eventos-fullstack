ad2fa97e7d5fb6198c77f8ec85617ef0357752df
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-14 18:29:25 -0400

    Remove commented code and clean up TicketController

--- microservices/ms-tickets/src/main/java/com/tickets/cl/ms_tickets/controller/TicketController.java
+++ microservices/ms-tickets/src/main/java/com/tickets/cl/ms_tickets/controller/TicketController.java
@@ -1,5 +1,5 @@
 
-// Recuerda utilizar la URL para el Postman: http://localhost:8082/tickets
+// Recuerda compañero utilizar la URL para el Postman: http://localhost:8082/tickets
 
 package com.tickets.cl.ms_tickets.controller;
 
@@ -18,27 +18,21 @@ public class TicketController {
     @Autowired
     private TicketService ticketService;
 
-    // 1. Obtener todos los Tickets
-    // Con esto le pediremos al sistema que muestre una lista con todos los tickest que estan
-    // guardado en la Base de Datos.
     @GetMapping
     public List<Ticket> listar() {
         return ticketService.obtenerTodos();
     }
 
-    // 2. Agregamos un nuevo ticket a la base de datos.
     @PostMapping
     public Ticket crear(@RequestBody Ticket ticket) {
         return ticketService.guardar(ticket);
     }
 
-    // 3. Busca un solo ticket usando la ID del ticket.
     @GetMapping("/{id}")
     public Ticket buscar(@PathVariable Integer id) {
         return ticketService.obtenerPorId(id);
     }
 
-    // 4. Borrarmos un ticket del sistema permanentemente.
     @DeleteMapping("/{id}")
     public void borrar(@PathVariable Integer id) {
         ticketService.eliminar(id);
