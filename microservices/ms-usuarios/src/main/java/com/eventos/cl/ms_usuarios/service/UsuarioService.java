5061bc468682f72ffe053e3308d483c53c761e0d
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-17 09:10:52 -0400

    Clean up comments in UsuarioService
    Removed comments from UsuarioService methods.

--- microservices/ms-usuarios/src/main/java/com/eventos/cl/ms_usuarios/service/UsuarioService.java
+++ microservices/ms-usuarios/src/main/java/com/eventos/cl/ms_usuarios/service/UsuarioService.java
@@ -13,22 +13,18 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    // Obtener la lista de todos los usuarios.
     public List<Usuario> obtenerTodos() {
         return usuarioRepository.findAll();
     }
 
-    // Agregar un nuevo usuario a la base de datos.
     public Usuario guardar(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
-    // Busca un usuario por ID.
     public Usuario obtenerPorId(Integer id) {
         return usuarioRepository.findById(id).orElse(null);
     }
 
-    // Eliminar un usuario de forma permanente.
     public void eliminar(Integer id){
         usuarioRepository.deleteById(id);
     }
