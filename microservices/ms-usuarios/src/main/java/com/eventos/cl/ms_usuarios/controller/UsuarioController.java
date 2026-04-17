37021b8d479b76ca0ed58bfcf32a2b91099df368
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-17 09:10:07 -0400

    Update UsuarioController.java
    Removed comments for user operations in UsuarioController.

--- microservices/ms-usuarios/src/main/java/com/eventos/cl/ms_usuarios/controller/UsuarioController.java
+++ microservices/ms-usuarios/src/main/java/com/eventos/cl/ms_usuarios/controller/UsuarioController.java
@@ -14,25 +14,21 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    // 1. Obtener todos los Usuarios,
     @GetMapping
     public List<Usuario> listar() {
         return usuarioService.obtenerTodos();
     }
 
-    // 2. Agregamos un nuevo usuario a la base de datos.
     @PostMapping
     public Usuario crear(@RequestBody Usuario usuario) {
         return usuarioService.guardar(usuario);
     }
 
-    // 3. Busca un usuario usando el ID.
     @GetMapping("/{id}")
     public Usuario buscar(@PathVariable Integer id) {
         return usuarioService.obtenerPorId(id);
     }
 
-    // 4. Borramos un usuario permanentemente.
     @DeleteMapping("/{id}")
     public void borrar(@PathVariable Integer id) {
         usuarioService.eliminar(id);
