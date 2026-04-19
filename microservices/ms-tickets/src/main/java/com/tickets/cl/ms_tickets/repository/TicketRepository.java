54450b94f28997f24ae66581c81ce40ab9d0afa5
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-18 21:32:04 -0400

    Add methods to TicketRepository for querying tickets

--- microservices/ms-tickets/src/main/java/com/tickets/cl/ms_tickets/repository/TicketRepository.java
+++ microservices/ms-tickets/src/main/java/com/tickets/cl/ms_tickets/repository/TicketRepository.java
@@ -1,12 +1,17 @@
 package com.tickets.cl.ms_tickets.repository;
 
-
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
 import com.tickets.cl.ms_tickets.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+    List<Ticket> findByTipo(String tipo);
+
+    @Query("SELECT t from Ticket t where t.precio <= :precioMax")
+    List<Ticket>buscarPorPrecioMaximo(@Param("precioMax") Integer precioMax);
 }
