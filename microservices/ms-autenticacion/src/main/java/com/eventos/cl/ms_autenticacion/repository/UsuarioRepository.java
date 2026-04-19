512e5a9a6279488ea8f7dcaad09a995bf2e64a2c
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-19 00:53:29 -0400

    Update UsuarioRepository.java

--- microservices/ms-autenticacion/src/main/java/com/eventos/cl/ms_autenticacion/repository/UsuarioRepository.java
+++ microservices/ms-autenticacion/src/main/java/com/eventos/cl/ms_autenticacion/repository/UsuarioRepository.java
@@ -2,14 +2,19 @@ package com.eventos.cl.ms_autenticacion.repository;
 
 import com.eventos.cl.ms_autenticacion.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    // Ahora con esta funcion buscamos a un usuario por su nombre para que se login.
     Optional<Usuario> findByUsername(String username);
+
+    @Query("SELECT u FROM Usuario u WHERE UPPER(u.rol) = UPPER(:rol)")
+    List<Usuario> findByRolDeUsuario(@Param("rol") String rol);
 }
