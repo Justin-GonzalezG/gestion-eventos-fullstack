a5efa264073be129b491cee46a41972aeca8bcf1
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-19 00:52:39 -0400

    Add endpoint to filter users by role
    Added a new endpoint to filter users by role.

--- microservices/ms-autenticacion/src/main/java/com/eventos/cl/ms_autenticacion/controller/UsuarioController.java
+++ microservices/ms-autenticacion/src/main/java/com/eventos/cl/ms_autenticacion/controller/UsuarioController.java
@@ -1,3 +1,5 @@
+// http://localhost:8081/api/auth <-- Este es el URL Benja para el Atuenticador de Usuarios
+
 package com.eventos.cl.ms_autenticacion.controller;
 
 import com.eventos.cl.ms_autenticacion.model.Usuario;
@@ -30,16 +32,22 @@ public class UsuarioController {
         Usuario usuario = usuarioService.obtenerPorId(id);
 
         if (usuario != null) {
-            return "Usuario encontrado: " + usuario.getNombre() + " " + usuario.getApellido();
+            return "Usuario encontrado: " + usuario.getNombre() + " " + usuario.getApellido() + " Rol: " + usuario.getRol();
         } else {
-            return "Error: No existe ningún usuario con el ID " + id;
+            return "No existe ningún usuario con el ID " + id;
         }
+    }
+
+    //Agregar al Github
+    @GetMapping("/filtrar/{rol}")
+    public List<Usuario> filtrarPorRol(@PathVariable String rol) {
+        return usuarioService.filtrarPorRol(rol);
     }
 
     @DeleteMapping("/{id}")
     public String borrar(@PathVariable Long id) {
         usuarioService.eliminar(id);
-        return "El usuario con ID " + id + " ha sido eliminado de la base de datos.";
+        return "El usuario con ID " + id + " ha sido eliminado con exito.";
     }
 
     @PostMapping("/login")
