85db2bbff1bf3cf234507c1995ed273cd8d0d5ed
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-04-27 20:21:02 -0400

    implementación inicial del modelo de datos para ms-ordenes

---
+++ microservices/ms-ordenes/src/main/java/com/eventos/cl/ms_ordenes/model/DetalleOrden.java
@@ -0,0 +1,30 @@
+package com.eventos.cl.ms_ordenes.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "detalles_orden")
+
+public class DetalleOrden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "orden_id", nullable = false)
+    private Orden orden;
+}
