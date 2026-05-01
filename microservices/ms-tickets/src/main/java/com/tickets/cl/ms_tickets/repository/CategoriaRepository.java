61d13fcfa949fd8cb7c5a62ed458a2a865c4c3ee
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-05-01 14:52:17 -0400

    Delete microservices/ms-tickets/src/main/java/com/tickets/cl/ms_tickets directory

--- microservices/ms-tickets/src/main/java/com/tickets/cl/ms_tickets/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tickets.cl.ms_tickets.repository;
-
-import com.tickets.cl.ms_tickets.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
