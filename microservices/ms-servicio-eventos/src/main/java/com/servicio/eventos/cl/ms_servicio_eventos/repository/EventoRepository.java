09728eee17c28e66cfa306dd5f9a897927eb1a37
Ben. <benjaminsalazar2111@gmail.com>
2026-05-11 22:55:53 -0400

    Delete microservices/ms-servicio-eventos directory

--- microservices/ms-servicio-eventos/src/main/java/com/servicio/eventos/cl/ms_servicio_eventos/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.servicio.eventos.cl.ms_servicio_eventos.repository;
-
-import com.servicio.eventos.cl.ms_servicio_eventos.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-}
