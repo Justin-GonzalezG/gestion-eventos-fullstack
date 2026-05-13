8c1d149ad456866703dd66a18435df98f437a618
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-05-13 01:57:06 -0400

    Refactor test class to main application class
    Refactored MsPagosApplicationTests to MsPagosApplication with main method and Spring Boot application annotations.

--- microservices/ms-pagos/src/test/java/cl/eventos/ms_pagos/MsPagosApplicationTests.java
+++ microservices/ms-pagos/src/test/java/cl/eventos/ms_pagos/MsPagosApplicationTests.java
@@ -1,13 +1,16 @@
 package cl.eventos.ms_pagos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootTest
-class MsPagosApplicationTests {
+@SpringBootApplication
+@EnableFeignClients
 
-	@Test
-	void contextLoads() {
+public class MsPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPagosApplication.class, args);
 	}
 
 }
