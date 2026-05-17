ca05fe7b90408cbf39bcd796d3bf31b992d3eefa
Ben. <benjaminsalazar2111@gmail.com>
2026-05-17 13:44:47 -0400

    Delete microservices/ms-soporte directory

--- microservices/ms-soporte/src/main/java/com/eventos/cl/ms_soporte/repository/TicketSoporteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventos.cl.ms_soporte.repository;
-
-import com.eventos.cl.ms_soporte.model.TicketSoporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketSoporteRepository extends JpaRepository<TicketSoporte, Long> {
-}
