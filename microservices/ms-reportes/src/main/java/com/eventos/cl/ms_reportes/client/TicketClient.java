a23c2d5bdd670199852c9579caff852ad8eb3a75
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 17:03:29 -0400

    Update TicketClient.java

--- microservices/ms-reportes/src/main/java/com/eventos/cl/ms_reportes/client/TicketClient.java
+++ microservices/ms-reportes/src/main/java/com/eventos/cl/ms_reportes/client/TicketClient.java
@@ -1,13 +1,13 @@
-package com.eventos.cl.ms_reportes.client;
+package cl.eventos.ms_reportes.client;
 
-import com.eventos.cl.ms_reportes.dto.TicketDTO;
+import cl.eventos.ms_reportes.dto.TicketDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-tickets", url = "${ms.tickets.url}")
+@FeignClient(name = "ms-tickets")
 public interface TicketClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/tickets/{id}")
     TicketDTO obtenerTicketPorId(@PathVariable("id") Long id);
 }
