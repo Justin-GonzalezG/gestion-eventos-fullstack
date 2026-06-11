cdd49165712729d00c3f06fa9c07f6d024c1c424
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 17:37:39 -0400

    Update OrdenClient.java

--- microservices/ms-soporte/src/main/java/com/eventos/cl/ms_soporte/client/OrdenClient.java
+++ microservices/ms-soporte/src/main/java/com/eventos/cl/ms_soporte/client/OrdenClient.java
@@ -1,13 +1,13 @@
-package com.eventos.cl.ms_soporte.client;
+package cl.eventos.ms_soporte.client;
 
-import com.eventos.cl.ms_soporte.dto.OrdenDTO;
+import cl.eventos.ms_soporte.dto.OrdenDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-ordenes", url = "${ms.ordenes.url}")
+@FeignClient(name = "ms-ordenes")
 public interface OrdenClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/ordenes/{id}")
     OrdenDTO obtenerOrdenPorId(@PathVariable("id") Long id);
 }
