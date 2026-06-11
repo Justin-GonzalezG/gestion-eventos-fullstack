8612b0c3925628f643fd12e0d3dbe7eaa3cf2c02
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 17:02:21 -0400

    Refactor OrdenClient package and update endpoint path

--- microservices/ms-reportes/src/main/java/com/eventos/cl/ms_reportes/client/OrdenClient.java
+++ microservices/ms-reportes/src/main/java/com/eventos/cl/ms_reportes/client/OrdenClient.java
@@ -1,14 +1,13 @@
-package com.eventos.cl.ms_reportes.client;
+package cl.eventos.ms_reportes.client;
 
-import com.eventos.cl.ms_reportes.dto.OrdenDTO;
+import cl.eventos.ms_reportes.dto.OrdenDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
 
-@FeignClient(name = "ms-ordenes", url = "${ms.ordenes.url}")
+@FeignClient(name = "ms-ordenes")
 public interface OrdenClient {
 
-    @GetMapping("/listar")
+    @GetMapping("/api/ordenes/listar")
     List<OrdenDTO> listarOrdenes();
 }
