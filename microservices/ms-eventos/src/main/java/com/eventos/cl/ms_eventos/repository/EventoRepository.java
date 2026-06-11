29483e0b362660fcb742db526ece0d0d1baeef8d
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 14:07:00 -0400

    Update EventoRepository.java

--- microservices/ms-eventos/src/main/java/com/eventos/cl/ms_eventos/repository/EventoRepository.java
+++ microservices/ms-eventos/src/main/java/com/eventos/cl/ms_eventos/repository/EventoRepository.java
@@ -1,9 +1,10 @@
-package com.eventos.cl.ms_eventos.repository;
+package cl.eventos.ms_eventos.repository;
 
-import com.eventos.cl.ms_eventos.model.Evento;
+import cl.eventos.ms_eventos.model.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
 }
+
