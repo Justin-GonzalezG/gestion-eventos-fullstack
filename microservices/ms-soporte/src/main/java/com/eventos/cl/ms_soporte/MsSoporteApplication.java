bcb726063c72a7e41bc80067ef0d4814f1e0e6ed
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 17:42:34 -0400

    Change package name from 'com.eventos' to 'cl.eventos'

--- microservices/ms-soporte/src/main/java/com/eventos/cl/ms_soporte/MsSoporteApplication.java
+++ microservices/ms-soporte/src/main/java/com/eventos/cl/ms_soporte/MsSoporteApplication.java
@@ -1,4 +1,4 @@
-package com.eventos.cl.ms_soporte;
+package cl.eventos.ms_soporte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
