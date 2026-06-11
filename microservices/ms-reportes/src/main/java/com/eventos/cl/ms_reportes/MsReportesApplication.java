60848e67be1369e1f474614491ae32f243988019
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 17:08:49 -0400

    Update MsReportesApplication.java

--- microservices/ms-reportes/src/main/java/com/eventos/cl/ms_reportes/MsReportesApplication.java
+++ microservices/ms-reportes/src/main/java/com/eventos/cl/ms_reportes/MsReportesApplication.java
@@ -1,4 +1,4 @@
-package com.eventos.cl.ms_reportes;
+package cl.eventos.ms_reportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
