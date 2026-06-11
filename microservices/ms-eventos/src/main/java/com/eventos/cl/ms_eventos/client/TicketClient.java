2f9d3255aceb9f703a3dc07e090a129219823bae
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 12:33:18 -0400

    Refactor package name for TicketClient

--- microservices/ms-eventos/src/main/java/com/eventos/cl/ms_eventos/client/TicketClient.java
+++ microservices/ms-eventos/src/main/java/com/eventos/cl/ms_eventos/client/TicketClient.java
@@ -1,6 +1,6 @@
-package com.eventos.cl.ms_eventos.client;
+package cl.eventos.ms_eventos.client;
 
-import com.eventos.cl.ms_eventos.dto.TicketDTO;
+import cl.eventos.ms_eventos.dto.TicketDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
