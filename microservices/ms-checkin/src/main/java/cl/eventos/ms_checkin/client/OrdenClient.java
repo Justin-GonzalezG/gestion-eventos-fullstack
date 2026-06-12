1cea56082fa8c5a0d00a90695c9e1521d1506313
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-11 21:23:37 -0400

    Update OrdenClient to use new API endpoint

--- microservices/ms-checkin/src/main/java/cl/eventos/ms_checkin/client/OrdenClient.java
+++ microservices/ms-checkin/src/main/java/cl/eventos/ms_checkin/client/OrdenClient.java
@@ -4,10 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-ordenes", url = "${config.ms-ordenes.url}")
+@FeignClient(name = "ms-ordenes")
 public interface OrdenClient {
 
-    @GetMapping("/validar-pago/{ticketId}")
+    @GetMapping("/api/ordenes/validar-pago/{ticketId}")
     boolean verificarPagoTicket(@PathVariable("ticketId") Long ticketId);
-    
 }
