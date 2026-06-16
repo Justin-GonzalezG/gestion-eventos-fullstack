67f764bb2045e1d33218dfaf0b082682f2407c7c
Ben. <benjaminsalazar2111@gmail.com>
2026-06-15 23:14:19 -0400

    Delete microservices/ms-recomendacion directory

--- microservices/ms-recomendacion/src/main/java/com/eventos/cl/ms_recomendacion/repository/RecomendacionRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.eventos.ms_recomendacion.repository;
-
-import cl.eventos.ms_recomendacion.model.Recomendacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecomendacionRepository extends JpaRepository<Recomendacion, Long> {
-}
