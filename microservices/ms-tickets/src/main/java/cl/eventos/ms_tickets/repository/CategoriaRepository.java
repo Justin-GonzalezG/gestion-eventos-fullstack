64b22bc83cb8e9080acb84433e6205eefe32c97b
Ben. <benjaminsalazar2111@gmail.com>
2026-06-15 23:46:05 -0400

    Delete microservices/ms-tickets directory

--- microservices/ms-tickets/src/main/java/cl/eventos/ms_tickets/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.eventos.ms_tickets.repository;
-
-import cl.eventos.ms_tickets.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
