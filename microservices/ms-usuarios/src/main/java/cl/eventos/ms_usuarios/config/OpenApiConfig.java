045fcbc48f1b94c8050b1f82a3e6dd91e810948f
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-17 01:08:36 -0400

    Add files via upload

---
+++ microservices/ms-usuarios/src/main/java/cl/eventos/ms_usuarios/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package cl.eventos.ms_usuarios.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Usuarios")
+                        .version("1.0")
+                        .description("Gestión de cuentas y perfiles de usuario"));
+    }
+}
