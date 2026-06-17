66af91ac2738383b6bda343854f5af005dc51ec5
Justin-GonzalezG <just.gonzalez@duocuc.cl>
2026-06-16 23:51:38 -0400

    Add files via upload

---
+++ microservices/ms-pagos/src/main/java/cl/eventos/ms_pagos/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package cl.eventos.ms_pagos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Pagos")
+                        .version("1.0")
+                        .description("Gestión de pasarelas y validación de pagos"));
+    }
+}
